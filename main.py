97858fe7a379b7e981d6bd166a1f700c25690ad2
lukaszharkot <70739792+lukaszharkot@users.noreply.github.com>
2023-05-16 09:31:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ class StudentCreateSchema(BaseModel):
     first_name: str
     last_name : str
 
-    class 
+    class StudentUpdateSchema:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
 
 @app.post("/students/", status_code=200)
 async def create_student(student: StudentCreateSchema):
     return student
 
-@app.get
+@app.get("/students/{student_id}")
+async def read_student(student_id):
+    if student_id not in students:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return {"student": students[student_id]}
 
 @app.get("/")
 async def root():
